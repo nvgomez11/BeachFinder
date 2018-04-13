46674355b66715c12986ac99130cd93544a4a60d
Nelson <nvgomez11@gmail.com>
2018-04-13 15:23:08 -0600

    Ya obtiene json de nuestro API

--- FrontEnd/app/src/main/java/com/example/nelson/beachfinder/beachList.java
+++ FrontEnd/app/src/main/java/com/example/nelson/beachfinder/beachList.java
@@ -14,8 +14,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.facebook.login.LoginManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -24,6 +42,7 @@ public class beachList extends AppCompatActivity
 
     ArrayList<String> data_user = data_user = new ArrayList<String>();;
     static ArrayList<String> data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +83,8 @@ public class beachList extends AppCompatActivity
                 }
             }
         }
+
+        request_json();
     }
 
     @Override
@@ -82,7 +103,6 @@ public class beachList extends AppCompatActivity
         getMenuInflater().inflate(R.menu.search, menu);
         return true;
     }
-
 
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -119,5 +139,55 @@ public class beachList extends AppCompatActivity
     public void go_selected_beach(View view){
         Intent intent = new Intent(this,SelectedBeach.class);
         startActivity(intent);
+    }
+
+    public void request_json(){
+        RequestQueue mRequestQueue;
+        //Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        //Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+        //Instantiate the RequestQueue with the cache and network.
+        mRequestQueue = new RequestQueue(cache, network);
+        //Start the queue
+        mRequestQueue.start();
+        // Initialize a new JsonArrayRequest instance
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                //ip de la maquina, cel y compu deben estar en misma red
+                "http://172.19.51.214:3000/beaches.json",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Do something with response
+                        // Process the JSON
+                        Log.d("mop",response.toString());
+                        try{
+                            // Loop through the array elements
+                            for(int i=0;i<response.length();i++){
+                                // Get current json object
+                                JSONObject beach = response.getJSONObject(i);
+
+                                String beachName = beach.getString("beach_name");
+                                String sand = beach.getString("sand_color");
+                                Log.d("mop",beachName);
+                                Log.d("mop",sand);
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        // Do something when error occurred
+                        Log.d("Error","No pudo entrar al link");
+                    }
+                }
+        );
+        // Adding request to request queue
+        mRequestQueue.add(jsonArrayRequest);
     }
 }
