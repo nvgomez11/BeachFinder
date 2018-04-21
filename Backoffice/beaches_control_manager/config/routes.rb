0d56119d035b9e18902e1f234321f4d11ea370e0
Edward Rodriguez <luisrova17@gmail.com>
2018-04-21 06:51:00 -0700

    Backend actualizado

--- Backoffice/beaches_control_manager/config/routes.rb
+++ Backoffice/beaches_control_manager/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   resources :beaches
   resources :users
+
+  get '/index', :to => static('index.html')
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
