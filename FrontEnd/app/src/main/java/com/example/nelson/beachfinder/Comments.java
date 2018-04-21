9d714d2a0eaa18d09c76ce7c39a6ac8b34e27108
Nelson <nvgomez11@gmail.com>
2018-04-21 02:10:15 -0600

    arreglo layout comments

--- FrontEnd/app/src/main/java/com/example/nelson/beachfinder/Comments.java
+++ FrontEnd/app/src/main/java/com/example/nelson/beachfinder/Comments.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,20 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 
-import java.util.ArrayList;
-
 public class Comments extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-
-    ArrayList<String> beach_CommentAutor = new ArrayList<String>();
-    ArrayList<String> beach_comment = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +25,6 @@ public class Comments extends AppCompatActivity
         setContentView(R.layout.activity_comments);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,42 +35,15 @@ public class Comments extends AppCompatActivity
             }
         });
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-
-
-        //-------------Mostrar comentario en GridView
-        GridView gridViewComments  = findViewById(R.id.grid_viewComments);
-        Intent intent = getIntent();
-        String comentarios = intent.getStringExtra("selected_beach");
-
-
-        String comentariosLista[]=comentarios.split(";");
-        //En este momento comentarios del API tiene forma así:
-        //Autor:[Comentario;Autor2:Comentario2]
-
-        for (String comentario:comentariosLista) {
-            String tempComent[]=comentario.split(":");
-            beach_comment.add(tempComent[1]);
-            beach_CommentAutor.add(tempComent[0]);
-        }
-
-/*
-        fill_titles_desc_icon();
-        for(int i=0; i<beaches_titles.size(); i++){
-            Log.d("met_beaches",beaches_titles.get(i));
-        }
-*/
-
-        GridCommentsAdapter adapterComment = new GridCommentsAdapter(this,beach_CommentAutor,beach_comment);
-        gridViewComments.setAdapter(adapterComment);
-
-
-
-
-
-        //
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
-
 
     @Override
     public void onBackPressed() {
@@ -111,7 +63,6 @@ public class Comments extends AppCompatActivity
     }
 
 
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -126,12 +77,8 @@ public class Comments extends AppCompatActivity
             startActivity(intent);
 
         } else if (id == R.id.nav_profile) {
-            Intent intent = new Intent(this,Profile.class);
-            startActivity(intent);
 
         } else if (id == R.id.nav_about) {
-            Intent intent = new Intent(this,About_section.class);
-            startActivity(intent);
 
         } else if (id == R.id.nav_sign_out) {
             //closes facebook session de facebook
